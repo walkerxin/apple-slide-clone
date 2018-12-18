a0dbd474fbda487df3f9871fa287abcbb314c3ec
walkerxin <circle_Web@163.com>
2018-12-18 17:33:34 +0800

    自动播放

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 let menu = $('.slide-menu>ul>li')
-
 for(let i=0; i<menu.length; i++) {
     $(menu[i]).on('click', function(e) {
         
         let idx = $(e.currentTarget).index()
-        $('#slide_img').css('transform','translateX(' + (-idx*920) + 'px)')
-        $(`.slide-menu>ul>li:nth(${ idx })`).addClass('active').siblings().removeClass('active')
+        playThisOne(idx)
+        n = idx
         
     })
+}
+
+// 自动轮播
+let n = 0;
+setInterval(() => {
+    n += 1
+    n = n % menu.length    
+    playThisOne(n)
+}, 4000)
+
+function playThisOne(n) {
+    $('#slide_img').css('transform','translateX(' + (-n*920) + 'px)')
+    $(`.slide-menu>ul>li:nth(${ n })`).addClass('active').siblings().removeClass('active')
 }
